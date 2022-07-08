9256e19369db8f580217d84868b91a021f124775
이겨레 <grlee@saltlux.com>
2022-07-08 09:02:46 +0900

    Initial commit

---
+++ src/main/java/com/example/gitlabtest/GitlabtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabtestApplication.class, args);
+	}
+
+}
